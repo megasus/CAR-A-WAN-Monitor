e8906bf1e74746bd507852cbd8cea27a2ed3500c
Alcanmage <geslauncher@gmx.de>
2014-11-13 13:02:36 +0100

    Initial creation

---
+++ CARAWAN/Form1.Designer.vb
@@ -0,0 +1,192 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.status_lbl = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout
+        Me.SuspendLayout
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 44)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(504, 424)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(3, 6)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(129, 34)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Hold/Continue"
+        Me.Button1.UseVisualStyleBackColor = true
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.Location = New System.Drawing.Point(522, 12)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(451, 353)
+        Me.RichTextBox2.TabIndex = 2
+        Me.RichTextBox2.Text = ""
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(667, 389)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(174, 42)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Send"
+        Me.Button2.UseVisualStyleBackColor = true
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = true
+        Me.Label1.Location = New System.Drawing.Point(9, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(20, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "IP:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(35, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(231, 20)
+        Me.TextBox1.TabIndex = 5
+        Me.TextBox1.Text = "192.168.2.1"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(307, 12)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(64, 20)
+        Me.TextBox2.TabIndex = 7
+        Me.TextBox2.Text = "46000"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = true
+        Me.Label2.Location = New System.Drawing.Point(272, 15)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Port:"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(377, 4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(129, 34)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Connect"
+        Me.Button3.UseVisualStyleBackColor = true
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(138, 6)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(129, 34)
+        Me.Button4.TabIndex = 9
+        Me.Button4.Text = "Clear"
+        Me.Button4.UseVisualStyleBackColor = true
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = true
+        Me.Label3.Location = New System.Drawing.Point(295, 17)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(40, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Status:"
+        '
+        'status_lbl
+        '
+        Me.status_lbl.AutoSize = true
+        Me.status_lbl.Location = New System.Drawing.Point(341, 17)
+        Me.status_lbl.Name = "status_lbl"
+        Me.status_lbl.Size = New System.Drawing.Size(33, 13)
+        Me.status_lbl.TabIndex = 11
+        Me.status_lbl.Text = "None"
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.status_lbl)
+        Me.Panel1.Controls.Add(Me.Button4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Location = New System.Drawing.Point(12, 469)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(504, 48)
+        Me.Panel1.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(979, 522)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.RichTextBox2)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Name = "Form1"
+        Me.Text = "CAR-A-WAN Monitor"
+        Me.Panel1.ResumeLayout(false)
+        Me.Panel1.PerformLayout
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents RichTextBox2 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents status_lbl As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+
+End Class
---
+++ CARAWAN/Form1.vb
@@ -0,0 +1,144 @@
+﻿Imports System.Net.Sockets
+Imports System.IO
+Imports Microsoft.VisualBasic.Devices
+Imports System.Text
+Imports System.Threading
+Imports System.Xml
+Imports System.Reflection
+Imports System.Runtime.Remoting
+
+Public Class Form1
+    Public globTcp As TcpClient
+    Public hold As Boolean = False
+    Dim InitSize As Size
+    Shared TrdLst As List(Of Thread)
+
+    Private Sub Form1_Closing(sender As Object, e As EventArgs) Handles MyBase.FormClosing
+        For Each trd As Thread In TrdLst
+            trd.Abort()
+        Next
+        If globTcp IsNot Nothing Then globTcp.Close()
+
+    End Sub
+    Private Sub Listen(tcp As TcpClient)
+        Dim bytes(1024) As Byte
+        Dim st As NetworkStream = tcp.GetStream()
+        Dim i As Int32
+        Dim data As String
+        ' Loop to receive all the data sent by the client.
+        i = st.Read(bytes, 0, bytes.Length)
+        While (i <> 0)
+            ' Translate data bytes to a ASCII string.
+            data = Nothing
+            data = Encoding.UTF8.GetString(bytes, 0, i)
+            RichTextBox1.BeginInvoke(New UpdateControl(AddressOf UpdateControlDelegate), RichTextBox1, data)
+            InterpreteXml(data)
+            i = st.Read(bytes, 0, bytes.Length)
+        End While
+    End Sub
+    Private Delegate Sub UpdateControl(control As Control, txt As String)
+    Private Sub UpdateControlDelegate(control As Control, txt As String)
+        If TypeOf (control) Is RichTextBox Then
+            Dim rt As RichTextBox = CType(control, RichTextBox)
+            If Not hold Then
+                If txt.Contains("<?xml") Then
+                    rt.AppendText("" & vbNewLine)
+                    rt.AppendText(New String("-", 100) & vbNewLine)
+                    rt.AppendText("" & vbNewLine)
+                    rt.AppendText(txt)
+                Else
+                    rt.AppendText(txt)
+                End If
+
+                rt.ScrollToCaret()
+            End If
+        ElseIf TypeOf (control) Is Label Then
+            Dim lb As Label = CType(control, Label)
+            lb.Text = txt
+        End If
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim st As NetworkStream = globTcp.GetStream()
+        Dim bytes As Byte() = Encoding.UTF8.GetBytes(RichTextBox2.Text)
+        RichTextBox2.Clear()
+        st.Write(bytes, 0, bytes.Length)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        hold = Not hold
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        For Each trd As Thread In TrdLst
+            trd.Abort()
+        Next
+        TrdLst = New List(Of Thread)()
+        If globTcp IsNot Nothing Then globTcp.Close()
+        Try
+            Dim tcp As New TcpClient(TextBox1.Text, CInt(TextBox2.Text)) '46000
+            Debug.WriteLine("Connected")
+            globTcp = tcp
+            Dim trd As New Thread(AddressOf Listen)
+            TrdLst.Add(trd)
+            trd.Start(tcp)
+        Catch ex As Exception
+            Debug.WriteLine("Failed")
+            MsgBox("Verbindung konnte nicht aufgebaut werden!", MsgBoxStyle.Critical, "Fehler")
+        End Try
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        RichTextBox1.Clear()
+    End Sub
+    Dim _fullXml As String
+    Private Sub InterpreteXml(xml As String)
+        _fullXml &= xml
+        If Not xml.Contains("</cawpacket>") Then
+            Exit Sub
+        End If
+        Dim statusTxt As String = ""
+        Using reader As XmlTextReader = New XmlTextReader(New StringReader(_fullXml))
+            Dim xmlDoc As New XmlDocument
+            xmlDoc.Load(reader)
+            Dim root As XmlElement = xmlDoc.DocumentElement
+            Dim nodes As XmlNodeList = root.GetElementsByTagName("module")
+            If Not nodes Is Nothing Then
+                For Each node As XmlNode In nodes
+                    If statusTxt.Length = 0 Then
+                        statusTxt &= node.Item("dialstatus").InnerText & "/"
+                    Else
+                        statusTxt &= node.Item("dialstatus").InnerText
+                    End If
+                Next
+                status_lbl.BeginInvoke(New UpdateControl(AddressOf UpdateControlDelegate), status_lbl, statusTxt)
+            End If
+        End Using
+        _fullXml = ""
+    End Sub
+    Dim initialized As Boolean = False
+    Dim container1size As Size
+    Dim container2size As Size
+    Private Sub Form1_SizeChange(sender As Object, e As EventArgs) Handles MyBase.SizeChanged
+        If initialized Then
+            RichTextBox1.Size = New Size(container1size.Width + ((Size.Width - InitSize.Width) / 2), container1size.Height + Size.Height - InitSize.Height)
+            RichTextBox2.Size = New Size(container2size.Width + ((Size.Width - InitSize.Width) / 2), container2size.Height + Size.Height - InitSize.Height)
+            RichTextBox2.Location = New Point(RichTextBox1.Width + 19, RichTextBox2.Location.Y)
+            Panel1.Location = New Point(Panel1.Location.X, RichTextBox1.Height + Panel1.Height)
+            Button2.Location = New Point(RichTextBox1.Width + 150, RichTextBox2.Height + 50)
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim d As DateTime = Now
+        Debug.WriteLine(d.ToShortDateString)
+        TrdLst = New List(Of Thread)()
+        InitSize = Size
+        container1size = RichTextBox1.Size
+        container2size = RichTextBox2.Size
+        Text = "CAR-A-WAN Monitor (" & ProductVersion.ToString() & ")"
+        initialized = True
+    End Sub
+End Class
